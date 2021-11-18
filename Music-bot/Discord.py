c9b348f0592ff30f424ff3eb3d9aaad35d48d65b
Cvargas17 <christianvargas2002@gmail.com>
2021-11-17 22:16:26 -0600

    Update Discord.py

--- Music-bot/Discord.py
+++ Music-bot/Discord.py
@@ -4,11 +4,12 @@ import music
 
 cogs = [musica]
 
-client = commands.Bot(command_prefix="!", intents = discord.Intents)
+client = commands.Bot(command_prefix="!", 
+intents = discord.Intents.all())
+
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
 
-
-client.run("OTEwNzIxNzkyOTA4MzQ5NDUw.YZW9ww.oO2TBV5VGymevX4E7ECdy54NhzQ")
+client.run("OTEwNzIxNzkyOTA4MzQ5NDUw.YZW9ww.v0egzSzk1cknO_3IEZP3mp2eKic")
